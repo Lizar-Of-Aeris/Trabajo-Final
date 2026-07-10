a33a220398124bedfee64eeb1d5b9db0b3eb08e2
Lizar-Of-Aeris <determinacion2004@gmail.com>
2026-07-10 10:48:50 -0500

    Desarrolla bucle For y agrega inputs

--- validacion_dulceria.py
+++ validacion_dulceria.py
@@ -8,17 +8,29 @@
 from datetime import date
 
 def registrar_compra():
+
+    #Solicita fecha
     fecha_compra = date.today()
 
     fecha_funcion = input("Ingrese la fecha de la función (AAAA-MM-DD): ")
 
-carrito = []
+    #Solicita cantidad de productos
+    carrito = [ ]
 
-cantidad = int(input("¿Cuántos productos agregará?: "))
+    cantidad = int(input("¿Cuántos productos agregará?: "))
 
-for i in range(cantidad):
+    #Desarrolla ciclo for
+    for i in range(cantidad):
 
-    print(f"\nProducto {i+1}")
+        print(f"\nProducto {i+1}")
+
+        nombre = input("Nombre: ")
+        tipo = input("Tipo (entrada/dulceria): ").lower()
+        precio = float(input("Precio: "))
 
 
+registrar_compra()
+
+
+    
 
