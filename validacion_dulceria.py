1cdc29ed68a415f2de0d9223211ec3e116aa6120
Lizar-Of-Aeris <determinacion2004@gmail.com>
2026-07-10 09:46:38 -0500

    Inicia ciclo for

--- validacion_dulceria.py
+++ validacion_dulceria.py
@@ -10,8 +10,15 @@ from datetime import date
 def registrar_compra():
     fecha_compra = date.today()
 
-    fecha_funcion = input("Ingrese la fecha de la función: ")
+    fecha_funcion = input("Ingrese la fecha de la función (AAAA-MM-DD): ")
+
+carrito = []
+
+cantidad = int(input("¿Cuántos productos agregará?: "))
+
+for i in range(cantidad):
+
+    print(f"\nProducto {i+1}")
 
 
 
-    
