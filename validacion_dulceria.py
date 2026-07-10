9e0488bd5474188606ccb8a3e36182155fe9b5eb
Lizar-Of-Aeris <determinacion2004@gmail.com>
2026-07-10 02:33:33 -0500

    Solicitud de fecha

---
+++ validacion_dulceria.py
@@ -0,0 +1,17 @@
+# ==========================================================
+# Trabajo Final Fundamentos de Programación I
+# registro.py
+# Integrante: Almendra Ximena Freyre Arriola
+# ==========================================================
+
+
+from datetime import date
+
+def registrar_compra():
+    fecha_compra = date.today()
+
+    fecha_funcion = input("Ingrese la fecha de la función: ")
+
+
+
+    
