480d2b97453e3b8687a8e786f9084702a34fbaa9
abigailhfzz22-crypto <abigailhfzz22@gmail.com>
2026-07-10 23:58:23 -0500

    Agrega menú principal y cartelera

---
+++ main.py
@@ -0,0 +1,17 @@
+# ==========================================
+# main.py
+# Integrante: Abigail Morales Ochoa
+# ==========================================
+
+from menu import menu
+
+
+def main():
+    opcion = menu()
+
+    if opcion == 1:
+        print("\nAquí continuará el sistema de compras...")
+
+
+if __name__ == "__main__":
+    main()
---
+++ menu.py
@@ -0,0 +1,44 @@
+# ==========================================
+# menu.py
+# Integrante: Abigail Morales Ochoa
+# ==========================================
+
+PELICULAS = {
+    1: "Lilo & Stitch",
+    2: "Jurassic World",
+    3: "Superman",
+    4: "Minecraft"
+}
+
+
+def mostrar_cartelera():
+    print("\n========== CARTELERA ==========")
+
+    for codigo, pelicula in PELICULAS.items():
+        print(codigo, "-", pelicula)
+
+
+def menu():
+    while True:
+        print("\n====================================")
+        print("      CINE UPC - AUTOSERVICIO")
+        print("====================================")
+        print("1. Realizar compra")
+        print("2. Ver cartelera")
+        print("3. Salir")
+
+        opcion = input("\nSeleccione una opción: ")
+
+        if opcion == "1":
+            return 1
+
+        elif opcion == "2":
+            mostrar_cartelera()
+            input("\nPresione ENTER para volver al menú...")
+
+        elif opcion == "3":
+            print("\nGracias por utilizar nuestro sistema.")
+            return 0
+
+        else:
+            print("\nOpción inválida. Intente nuevamente.")
