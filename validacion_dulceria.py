15e6c7f26a78bc79af0625cfa98f70f3854afd72
nicolasferrer2244-byte <nicolasferrer2244@gmail.com>
2026-07-11 00:29:04 -0500

    avance

---
+++ validacion_dulceria.py
@@ -0,0 +1,9 @@
+# ==========================================
+# validacion_dulceria.py
+# Integrante: Frank Nicolás Ferrer Beteta
+# ==========================================
+
+def validar_compra(
+        fecha_compra,
+        fecha_funcion,
+        carrito):
