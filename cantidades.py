b54cadb5cc6d159f062039bbf021536bfe6c2491
nicolasferrer2244-byte <nicolasferrer2244@gmail.com>
2026-07-11 20:45:59 -0500

    inicial

---
+++ cantidades.py
@@ -0,0 +1,12 @@
+# ==========================================
+# cantidades.py
+# Integrante :Frank Nicolas Ferrer
+# ==========================================
+
+PRODUCTOS_DULCERIA = {
+    1: ("Canchita grande", 18.00),
+    2: ("Combo familiar", 35.00),
+    3: ("Gaseosa", 10.00),
+    4: ("Hot dog", 12.00),
+    5: ("Nachos", 16.00)
+}
